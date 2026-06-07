bbda0cdca3fb3a48404327feea04ca640eac842f
moge_du <39789990+Snowoctopus@users.noreply.github.com>
2026-06-07 16:12:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def get_notion_data():
             url = props["url"]["rich_text"][0]["plain_text"] if props["url"]["rich_text"] else ""
             time = props["时间"]["date"]["start"] if props["时间"]["date"] else ""
             source = props["来源"]["rich_text"][0]["plain_text"] if props["来源"]["rich_text"] else "未知来源"
+            st.write(f"正在读取来源: '{source}'")
             source = source.replace("=", "") # 去掉难看的等号
             
             results.append({
